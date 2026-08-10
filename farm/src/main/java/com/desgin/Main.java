2e7e139e60ea2233552220053851247bfe121052
harshal-upare <upareharshal22@gmail.com>
2026-08-10 17:43:08 +0530

    1st to dev commit

---
+++ farm/src/main/java/com/desgin/Main.java
@@ -0,0 +1,12 @@
+package com.desgin;
+
+import com.desgin.view.WelcomePage;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Application.launch(WelcomePage.class,args);
+    }
+}
